54eeb132e751a92eccbe2e4d90a8c89e3bf6bb63
yuranich <yuri.a.samarin@gmail.com>
2014-11-27 17:22:17 -0430

    Nikolai Smirnov : One mote TestVector added (from http://habrahabr.ru/post/188152/ for 256 bits) also has wrong result.

--- src/mipt/infosec/bitcoin/test/TestStribog.java
+++ src/mipt/infosec/bitcoin/test/TestStribog.java
@@ -47,15 +47,45 @@ public static void main(String[] args) {
 					               (byte)0x97, (byte)0x53, (byte)0x7f, (byte)0xa8, 
 					               (byte)0x5d, (byte)0x9e, (byte)0x40, (byte)0x90, 
 					               (byte)0x4e, (byte)0xfe, (byte)0xd2, (byte)0x9d };
+			
+			byte[] testInput2 = { (byte)0x32, (byte)0x31, (byte)0x30, (byte)0x39, 
+					              (byte)0x38, (byte)0x37, (byte)0x36, (byte)0x35, 
+					              (byte)0x34, (byte)0x33, (byte)0x32, (byte)0x31, 
+					              (byte)0x30, (byte)0x39, (byte)0x38, (byte)0x37,  
+					              (byte)0x36, (byte)0x35, (byte)0x34, (byte)0x33, 
+					              (byte)0x32, (byte)0x31, (byte)0x30, (byte)0x39, 
+					              (byte)0x38, (byte)0x37, (byte)0x36, (byte)0x35, 
+					              (byte)0x34, (byte)0x33, (byte)0x32, (byte)0x31,  
+					              (byte)0x30, (byte)0x39, (byte)0x38, (byte)0x37, 
+					              (byte)0x36, (byte)0x35, (byte)0x34, (byte)0x33, 
+					              (byte)0x32, (byte)0x31, (byte)0x30, (byte)0x39, 
+					              (byte)0x38, (byte)0x37, (byte)0x36, (byte)0x35,  
+					              (byte)0x34, (byte)0x33, (byte)0x32, (byte)0x31, 
+					              (byte)0x30, (byte)0x39, (byte)0x38, (byte)0x37, 
+					              (byte)0x36, (byte)0x35, (byte)0x34, (byte)0x33, 
+					              (byte)0x32, (byte)0x31, (byte)0x30 };
+			byte[] testOutput2 = { (byte)0x00, (byte)0x55, (byte)0x7B, (byte)0xE5, 
+					               (byte)0xE5, (byte)0x84, (byte)0xFD, (byte)0x52, 
+					               (byte)0xA4, (byte)0x49, (byte)0xB1, (byte)0x6B, 
+					               (byte)0x02, (byte)0x51, (byte)0xD0, (byte)0x5D, 
+					               (byte)0x27, (byte)0xF9, (byte)0x4A, (byte)0xB7, 
+					               (byte)0x6C, (byte)0xBA, (byte)0xA6, (byte)0xDA, 
+					               (byte)0x89, (byte)0x0B, (byte)0x59, (byte)0xD8, 
+					               (byte)0xEF, (byte)0x1E, (byte)0x15, (byte)0x9D };
+			
 			byte[] hash1 = stribog.getHash(mes1);
 			byte[] hash2 = stribog.getHash(mes2);
 			
 			byte[] testHash1 = stribog.getHash(testInput1);
+			byte[] testHash2 = stribog.getHash(testInput2);
+			
 			System.out.println("Messages are hashed. The result is:\n" + 
-							   example1 + " \n " + Arrays.toString(hash1) + " \n " +
-							   example2 + " \n " + Arrays.toString(hash2) + " \n " +
-							   testInput1 + " = " + Arrays.toString(testHash1) + " \n "
-							   + "should be  = " + Arrays.toString(testOutput1));
+							   example1 + " \n " + Arrays.toString(hash1) + " \n" +
+							   example2 + " \n " + Arrays.toString(hash2) + " \n" +
+							   testInput1 + " = " + Arrays.toString(testHash1) + " \n" +
+							   "should be  = " + Arrays.toString(testOutput1) + "\n\n" +
+							   testInput2 + " = " + Arrays.toString(testHash2) + " \n " +
+							   "should be  = " + Arrays.toString(testOutput2));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
